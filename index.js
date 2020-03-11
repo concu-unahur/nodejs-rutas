fa435a70204b1b27e13225d2de4b1085ae693f92
Federico Aloi <federico.aloi@gmail.com>
2020-03-11 18:34:26 -0300

    Agregada versión con promises

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const fs = require('fs')
+const fs = require('fs');
 const MapQuest = require('./lib/map_quest');
 
 const api = new MapQuest('SIDB6cYO7U1HSQmLWnJgsZyGxujuUAPc');
 
-api.obtenerRuta(
-  'Casullo 1034, Morón, Buenos Aires',
+function escribirJSON(archivo, objeto) {
+  fs.writeFileSync(archivo, JSON.stringify(objeto, undefined, 2));
+}
+
+// Versión con callback (tres parámetros)
+api.obtenerRutaCallback(
+  'Casullo 1000, Morón, Buenos Aires',
   'Universidad Nacional de Hurlingham',
-  (err, direcciones) => {
+  (err, ruta) => {
     if (err) {
       throw err;
     }
 
-    console.log('Guardando ruta en archivo...')
-    fs.writeFileSync('ruta.json', JSON.stringify(direcciones, undefined, 2))      
+    console.log('Guardando ruta en archivo (via callback)...');
+    escribirJSON('ruta-callback.json', ruta);
   }
 );
+
+// Versión promise (dos parámetros)
+api
+  .obtenerRutaPromise(
+    'Casullo 1000, Morón, Buenos Aires',
+    'Universidad Nacional de Hurlingham'
+  )
+  .then(ruta => {
+    console.log('Guardando ruta en archivo (via promise)...');
+    escribirJSON('ruta-promise.json', ruta);
+  })
+  .catch(err => {
+    throw err;
+  });
