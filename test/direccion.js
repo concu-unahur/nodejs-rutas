46cddb06a37686fad7ec80d62002142c43c4155e
Federico Aloi <federico.aloi@gmail.com>
2020-03-15 21:05:46 -0300

    Mejorado output de script direccion

--- test/direccion.js
+++ test/direccion.js
@@ -1,12 +1,12 @@
 const MapQuest = require('../lib/map_quest');
 
-const api = new MapQuest('SIDB6cYO7U1HSQmLWnJgsZyGxujuUAPc');
+const api = new MapQuest('poner acá el api key');
 const direccion = process.argv[2];
 
 api
   .obtenerLugarPromise(direccion)
   .then(lugar => {
-    console.log('¡Dirección válida!');
+    console.log('¡Dirección válida!\nComprobá que la ciudad (adminArea5) y provincia (adminArea3) sean las que vos pensaste:');
     console.log(lugar);
   })
   .catch(error => {
